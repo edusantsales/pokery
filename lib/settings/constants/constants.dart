05c1df8935bc23df3a6eb28702a117f649b7f7e7
Eduardo Sales <eduardo.sales@compasso.com.br>
2023-03-14 02:10:51 -0300

    Adicionado as constantes de cor e padding do projeto

---
+++ lib/settings/constants/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// [Constantes]
+const double kPadding = 8.0;
+const Color kGreyColor = Color(0xFF424242);
+const Color kWhiteColor = Colors.white;
+const Color kRedColor = Color(0xFFFF1744);
+const Color kGreenColor = Color(0xFF00E676);
+
+/// [Getters]
+Color get kGreyColor80 => kGreyColor.withOpacity(0.8);
+Color get kGreyColor60 => kGreyColor.withOpacity(0.6);
+Color get kGreyColor40 => kGreyColor.withOpacity(0.4);
