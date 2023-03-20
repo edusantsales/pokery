5c6fb8c22103bbb41615c4d2dcc240d92611c948
Eduardo Sales <eduardo.sales@compasso.com.br>
2023-03-20 10:41:51 -0300

    Ordenação dos atributos da classe HttpResponseModel

--- lib/domain/models/http_response_model.dart
+++ lib/domain/models/http_response_model.dart
@@ -1,28 +1,28 @@
 class HttpResponseModel {
   HttpResponseModel({
     this.body,
+    this.headers,
+    this.path,
+    this.queryParameters,
     this.statusCode,
     this.statusMessage,
-    this.path,
-    this.headers,
-    this.queryParameters,
   });
 
   /// Http response body.
   final dynamic body;
+
+  /// Http request headers.
+  final Map<String, dynamic>? headers;
+
+  /// Http request path.
+  final String? path;
+
+  /// Http request query parameters.
+  final Map<String, dynamic>? queryParameters;
 
   /// Http response status code.
   final int? statusCode;
 
   /// Returns the reason phrase associated with the status code.
   final String? statusMessage;
-
-  /// Http request path.
-  final String? path;
-
-  /// Http request headers.
-  final Map<String, dynamic>? headers;
-
-  /// Http request query parameters.
-  final Map<String, dynamic>? queryParameters;
 }
